41fb936eed26b3c2bf8369b5876fd366c7d17929
Andy_4sberg <46200111+Andy-4sberg@users.noreply.github.com>
2021-01-31 23:13:15 +0000

    better command detection & new commands (alpha)

--- Main.java
+++ Main.java
@@ -1,21 +1,41 @@
 import java.util.HashMap;
 import java.util.Scanner;
-class Main {
+class SimpleScriptX {
 	public static void main(String[] args) {
-		//int line = 0;
+		int line = 0;
 		HashMap<String,Integer> keywords = new HashMap<>();
 		keywords.put("print",0);
+		keywords.put("help",1);
+		keywords.put("",2);
+		keywords.put("*",3);
+		keywords.put("clear",4);
 		Scanner raw_input = new Scanner(System.in);
 		System.out.print("\033[32;1m$ \033[34;1m");
 		String input = raw_input.nextLine();
 		System.out.println(input);
-		if(keywords.get(input.split(" ")[0])==null) {
-			System.out.printf("\033[31;1mSyntaxError in \"%s\"", input.split(" ")[0]);
+		if (keywords.get(input)==null) {
+			System.out.printf("\033[31;1mSyntaxError on line %s %n",line);
 		} else {
-			System.out.println("Found");
+			switch (keywords.get(input)) {
+				case 0:
+					System.out.println("Found!");
+					break;
+				case 1:
+					System.out.println(keywords);
+					break;
+				case 2:
+					System.out.println("BLANK");
+					break;
+				case 3:
+					System.out.println("COMMENT");
+					break;
+				case 4:
+					System.out.println("\033[A\033[2J");
+				default:
+					System.out.printf("\033[31;1mError whil processing line %i",line);
+					break;
+			}
+		System.out.println("\033[0;0m");
 		}
-	}/*
-	public static String execute(String args, int line) {
-		return "";
-	}*/
-} 
+	}
+}
