b04270185895744f44d1f6f5ef3c45d7dafb5746
Andy_4sberg <46200111+Andy-4sberg@users.noreply.github.com>
2021-02-04 17:47:34 +0000

    Faster user input, cleaned up source code

--- Main.java
+++ Main.java
@@ -1,40 +1,71 @@
+// Packages
+import java.io.BufferedReader;
 import java.util.HashMap;
-import java.util.Scanner;
-class SimpleScriptX {
-	public static void main(String[] args) {
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+// Main class [SimpleScriptX doesn't work]
+class Main {
+
+	// User input
+	public static void main(String[] args) throws IOException {
+
+		// Version
+		System.out.println("\033[34;1mSimpleScript X (Java) v1.0.2a");
+
+		// Line
 		int line = 0;
-		HashMap<String,Integer> keywords = new HashMap<>();
+
+		// BufferedReader input
+		BufferedReader raw_input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("\033[32;1m$ \033[34;1m");
+		String input = raw_input.readLine();
+
+		// [DEBUG] Print input
+		System.out.println(input);
+
+		// Execute input
+		execute(input, line);
+	}
+
+	// Execute command
+	public static void execute(String input, int line) {
+
+		// HashMap setup
+		HashMap<String,Integer> keywords = new HashMap<String,Integer>();
 		keywords.put("print",0);
 		keywords.put("help",1);
 		keywords.put("",2);
 		keywords.put("*",3);
 		keywords.put("clear",4);
-		Scanner raw_input = new Scanner(System.in);
-		System.out.print("\033[32;1m$ \033[34;1m");
-		String input = raw_input.nextLine();
-		System.out.println(input);
+
+		// Unknown command
 		if (keywords.get(input)==null) {
 			System.out.printf("\033[31;1mSyntaxError on line %s %n",line);
-		} else {
+		}
+		// Known command switch
+		else {
 			switch (keywords.get(input)) {
-				case 0:
+				case 0: // print
 					System.out.println("Found!");
 					break;
-				case 1:
+				case 1: // help
 					System.out.println(keywords);
 					break;
-				case 2:
-					System.out.println("BLANK");
+				case 2: // empty line
+					// System.out.println("BLANK");
 					break;
-				case 3:
-					System.out.println("COMMENT");
+				case 3: // comment
+					//System.out.println("COMMENT");
 					break;
-				case 4:
+				case 4: // clear
 					System.out.println("\033[A\033[2J");
-				default:
+				default: // if glitch
 					System.out.printf("\033[31;1mError whil processing line %i",line);
 					break;
 			}
+		
+		// Removes\ color
 		System.out.println("\033[0;0m");
 		}
 	}
